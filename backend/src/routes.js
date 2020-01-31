5b187393020d75a101026e8a1070a2a9b4af3522
guuhx97 <gustavorobertotb@outlook.com>
2020-01-30 22:25:50 -0300

    :sparkles: Feat: Added authentication by token jwt

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-// rotas
+routes.post('/sessions', SessionController.store);
 
 export default routes;
