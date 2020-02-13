4886f1b66c66fbd4bb981a45f5b700800f344b4f
guuhx97 <gustavorobertotb@outlook.com>
2020-02-13 16:40:32 -0300

    :memo: Feat: created function filter

--- backend/src/app/controllers/StatusController.js
+++ backend/src/app/controllers/StatusController.js
@@ -1,4 +1,7 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
 import Delivery from '../models/Delivery';
 import Deliveryman from '../models/Deliveryman';
 
@@ -38,13 +41,31 @@ class StatusController {
     const schema = Yup.object().shape({
       start_date: Yup.date(),
       end_date: Yup.date(),
-      signature_id: Yup.number().when('end_date', (end_date, field) =>
-        end_date ? field.require() : field
-      ),
+      signature_id: Yup.number(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation is fail' });
+    }
+
+    /**
+     * Check start_date already passed on
+     */
+    const startDate = parseISO(req.body.start_date);
+    if (isBefore(startDate, new Date())) {
+      return res
+        .startus(400)
+        .json({ erro: 'The start hour informed already passed on' });
+    }
+
+    /**
+     * Check end_date already passed on
+     */
+    const endDate = parseISO(req.body.start_date);
+    if (isBefore(endDate, new Date())) {
+      return res
+        .startus(400)
+        .json({ erro: 'The end hour informed already passed on' });
     }
 
     const delivery = Delivery.findByPk(req.params.id);
