ddac08748a1c2e920749668462d07b2c1d13cc3b
Gustavo <gustavorobertotb@outlook.com>
2020-02-18 23:40:57 -0300

    :rocket: Feat: Adds CRUD for admin users

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ ok: true }));
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+routes.put('/users/:user_id', UserController.update);
+routes.delete('/users/:user_id', UserController.delete);
+
+routes.post('/session', SessionController.store);
 
 export default routes;
