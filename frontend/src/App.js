f3d346051fbed1b1436fde0151ccba535440a5a1
guuhx97 <gustavorobertotb@outlook.com>
2020-07-03 00:39:22 -0300

    :construction: Feat: Adiciona roteamento as páginas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Routes from './routes';
+import history from './services/history';
+import GlobalStyles from './styles/global';
 
-function App() {
-  return <h1>Hello World</h1>;
+export default function App() {
+  return (
+    <Router history={history}>
+      <Routes />
+      <GlobalStyles />
+      <ToastContainer autoClose={3000} />
+    </Router>
+  );
 }
-
-export default App;
